de05349e3e810a757dcbe558d9572a9d080e92c9
Vishal Prajapati <vishalp951295@gmail.com>
2024-11-14 19:45:50 +0530

    Add project files.

---
+++ InfoIdentity/Controllers/MyController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfoIdentity.Controllers
+{
+    public class MyController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public MyController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        public MyController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public MyController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InfoIdentity/wwwroot/js/Eye_Toggle.js
@@ -0,0 +1,15 @@
+﻿
+    const passwordField = document.getElementById("password");
+    const togglePassword = document.querySelector(".password-toggle-icon i");
+
+    togglePassword.addEventListener("click", function () {
+        if (passwordField.type === "password") {
+        passwordField.type = "text";
+            togglePassword.classList.remove("ri-eye-off-line");
+            togglePassword.classList.add("ri-eye-line");
+        } else {
+        passwordField.type = "password";
+            togglePassword.classList.remove("ri-eye-line");
+            togglePassword.classList.add("ri-eye-off-line");
+        }
+    });
